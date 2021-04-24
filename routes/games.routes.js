d2c7be4865e831a17a391accb50e4e59ff857210
Bea Avis <bea.avis.p@gmail.com>
2021-04-24 21:09:49 +0200

    Update search route

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const router  = express.Router();
-const Game = require('../models/Game.model'); 
+const express = require("express");
+const router = express.Router();
+const Game = require("../models/Game.model");
 
 /* GET Games Page */
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   Game.find({})
-  .then(games => res.render('games/game-list', { games }))
-  .catch(error => next(error));  
+    .then((games) => res.render("games/game-list", { games }))
+    .catch((error) => next(error));
 });
 
-router.get('/search', (req, res, next) => {
+router.get("/search", (req, res, next) => {
   const { search } = req.query;
-  Game.find({ "name": {$regex : `.*(?i)${search}.*`} })
-  .then(games => res.render('games/game-list', { games, search }))
-  .catch(error => next(error));  
+  if (search) {
+    Game.find({ name: { $regex: `.*(?i)${search}.*` } })
+      .then((games) => res.render("games/game-list", { games, search }))
+      .catch((error) => next(error));
+  } else {
+    res.redirect("/games");
+  }
 });
 
 module.exports = router;

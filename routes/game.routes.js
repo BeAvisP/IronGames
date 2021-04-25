282206c033106d238adee03c28361324f972bb69
Bea Avis <bea.avis.p@gmail.com>
2021-04-25 12:26:26 +0200

    Add game to user collection

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const Game = require('../models/Game.model'); 
+const User =  require('../models/User.model');
 
 /* GET Games Page */
 
@@ -10,5 +11,21 @@ router.get('/:id', (req, res, next) => {
   .then(game => res.render('games/game-details', { game }))
   .catch(error => next(error));  
 });
+
+router.post('/add-collection', (req, res, next) => {
+  const { gameID } = req.body;
+  const { _id: userID } = req.user;
+  User.findById(userID)
+  .then((user) => {
+    if(user){
+      const { gameList } = user;
+      gameList.push(gameID);
+      User.findByIdAndUpdate(userID, { gameList }, { new: true })
+      .then((user) => res.redirect(`/game/${gameID}`))
+      .catch( error => next(error));
+    }
+  })
+  .catch(error => next(error));
+})
 
 module.exports = router;

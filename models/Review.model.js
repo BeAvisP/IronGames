b9cfda07e4539b990dc867f87dac7e58bfeb4f22
Bea Avis <bea.avis.p@gmail.com>
2021-04-25 15:36:34 +0200

    Fix timestamps in review model

--- models/Review.model.js
+++ models/Review.model.js
@@ -1,14 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema({
-  user: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  game: [{ type: Schema.Types.ObjectId, ref: "Game" }],
-  comment: { type: String },
-  timestamp: { type: Date },
-  upvote: { type: Number },
-  downvote: { type: Number },
-});
+const reviewSchema = new Schema(
+  {
+    user: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    game: [{ type: Schema.Types.ObjectId, ref: "Game" }],
+    comment: { type: String },
+    upvote: { type: Number },
+    downvote: { type: Number },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 const Review = mongoose.model("Review", reviewSchema);
 

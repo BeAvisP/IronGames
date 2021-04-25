f58f509aacae797994af7aa58e4fe4afe7b45162
Bea Avis <bea.avis.p@gmail.com>
2021-04-25 22:12:44 +0200

    Update views.config with favicon and partials path

--- configs/views.config.js
+++ configs/views.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const hbs = require("hbs");
 const express = require("express");
+const favicon = require('serve-favicon');
 
 module.exports = (app) => {
   app.set("views", path.join(__dirname, "..", "views"));
   app.set("view engine", "hbs");
   app.use(express.static(path.join(__dirname, "..", "public")));
-  /* app.use(
+  hbs.registerPartials(path.join(__dirname, "..", "views/partials"));
+  app.use(
     favicon(path.join(__dirname, "..", "public", "images", "favicon.ico"))
-  ); */
+  ); 
 };

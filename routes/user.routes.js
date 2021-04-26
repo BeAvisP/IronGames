f57a2ff5fb770a282c4231660c26b360e45c47fc
Lorgio Roda <lorgioroda@Lorgios-MacBook-Air.local>
2021-04-26 03:21:46 -0400

    Profile wishlist update⚡️

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User.model');
+const User = require("../models/User.model");
 
-router.get('/:id',(req, res) => {
+//GameList and Wishlist
+router.get("/:id", (req, res) => {
   const { id } = req.params;
   User.findById(id)
-  .populate('gameList')
-  .then((user) => {
-    res.render('auth/profile', {user})
-  })
-})
+    .populate("gameList")
+    .populate("wishlist")
+    .then((user) => {
+      res.render("auth/profile", { user });
+    });
+});
+
 
 
 module.exports = router;

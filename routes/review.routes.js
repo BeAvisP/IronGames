d85f71b75d9a800dac286a1e98d051d5cd75f904
Bea Avis <bea.avis.p@gmail.com>
2021-04-27 10:47:59 +0200

    Remove unnecesary require in review.routes

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Review = require("../models/Review.model");
-const Game = require("../models/Game.model");
-const User = require("../models/User.model");
 
 //TODO IsLoggedIn
 router.post("/create", (req, res, next) => {

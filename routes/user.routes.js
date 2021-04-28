5b46f659829b356dc7a054e88a7e7c035672794d
Lorgio Roda <lorgioroda@Lorgios-MacBook-Air.local>
2021-04-28 05:02:55 -0400

    Add reviews profile🚧

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,14 +11,19 @@ router.get("/:id/collection", (req, res) => {
   User.findById(id)
     .populate("gameList")
     .then((user) => {
-      if(JSON.stringify(req.user._id)===JSON.stringify(id)){
+      if (JSON.stringify(req.user._id) === JSON.stringify(id)) {
         authUser = true;
-          mappedGames = user.gameList.map((game)=> {
+        mappedGames = user.gameList.map((game) => {
           game.authUser = true;
           return game;
         });
       }
-      res.render("user/user-collection", { user, sessionUser: req.user, authUser, mappedGames });
+      res.render("user/user-collection", {
+        user,
+        sessionUser: req.user,
+        authUser,
+        mappedGames,
+      });
     });
 });
 
@@ -30,32 +35,36 @@ router.get("/:id/wishlist", (req, res) => {
   User.findById(id)
     .populate("wishlist")
     .then((user) => {
-      if(JSON.stringify(req.user._id)===JSON.stringify(id)){
+      if (JSON.stringify(req.user._id) === JSON.stringify(id)) {
         authUser = true;
-          mappedWishlist = user.wishlist.map((wishlist)=> {
+        mappedWishlist = user.wishlist.map((wishlist) => {
           wishlist.authUser = true;
           return wishlist;
         });
       }
-      res.render("user/user-wishlist", { user, sessionUser: req.user, authUser, mappedWishlist });
+      res.render("user/user-wishlist", {
+        user,
+        sessionUser: req.user,
+        authUser,
+        mappedWishlist,
+      });
     });
 });
 
 //Profile reviews
-router.get('/:id/reviews', (req, res) => {
-  res.render('user/user-reviews', {sessionUser: req.user})
-})
-
+router.get("/:id/reviews", (req, res) => {
+  res.render("user/user-reviews", { sessionUser: req.user });
+});
 
 //Profile Game List and Wishlist
 router.get("/:id", (req, res) => {
   let authUser = false;
   const { id } = req.params;
   User.findById(id)
-    .populate({path:"gameList", options:{limit: 5}})
-    .populate({path:"wishlist", options:{limit: 5}})
+    .populate({ path: "gameList", options: { limit: 5 } })
+    .populate({ path: "wishlist", options: { limit: 5 } })
     .then((user) => {
-      if(JSON.stringify(req.user._id) === JSON.stringify(id)){
+      if (JSON.stringify(req.user._id) === JSON.stringify(id)) {
         authUser = true;
       }
       res.render("user/profile", { user, sessionUser: req.user, authUser });
